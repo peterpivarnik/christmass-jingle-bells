28598750a7e59dbd5edaf7d863a55ff1de0e0350
Peter Pivarnik <peter.pivarnik.ext@porscheinformatik.com>
2022-12-23 21:45:00 +0100

    Cleaned code

--- src/main/java/com/psw/jingle/ChristmasJingleBells.java
+++ src/main/java/com/psw/jingle/ChristmasJingleBells.java
@@ -1,48 +1,53 @@
 package com.psw.jingle;
 
-import java.io.IOException;
-import java.net.URL;
-import java.time.Duration;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
 
 public class ChristmasJingleBells {
 
-    private static final String JINGLE_BELLS_SOUND_LOCATION = "/jingle-bells.wav";
-    private static final int BEEP_DURATION_SHORT = 10000;
+  public static final long WAITING_TIME = Duration.ofMinutes(15).toMillis();
+  public static final long SOUND_DURATION = 10000L;
+  private static final String JINGLE_BELLS_FILE = "/jingle-bells.wav";
 
-    public static void main(String[] attrs) {
-        ChristmasJingleBells christmasJingleBells = new ChristmasJingleBells();
-        christmasJingleBells.ringBells();
+  public static void main(String[] attrs) {
+    ChristmasJingleBells christmasJingleBells = new ChristmasJingleBells();
+    christmasJingleBells.ringBells();
+  }
+
+  private void ringBells() {
+    try {
+      Thread.sleep(WAITING_TIME);
+      URL soundUrl = getSoundUrl();
+      AudioInputStream inputStream = AudioSystem.getAudioInputStream(soundUrl);
+      DataLine.Info info = new DataLine.Info(Clip.class, inputStream.getFormat());
+      playSound(inputStream, info);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    private void ringBells() {
-        try {
-            sleep(Duration.ofMinutes(30));
-            URL soundURL = this.getClass().getResource(JINGLE_BELLS_SOUND_LOCATION);
-            AudioInputStream inputStream = AudioSystem.getAudioInputStream(soundURL);
-            DataLine.Info info = new DataLine.Info(Clip.class, inputStream.getFormat());
-            Clip clip = (Clip) AudioSystem.getLine(info);
-            clip.open(inputStream);
-            clip.start();
-            sleep(Duration.ofMillis(BEEP_DURATION_SHORT));
-            clip.stop();
-        } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
-            e.printStackTrace();
-        }
+  private void playSound(AudioInputStream inputStream, DataLine.Info info)
+      throws LineUnavailableException, IOException, InterruptedException {
+    Clip clip = (Clip) AudioSystem.getLine(info);
+    clip.open(inputStream);
+    clip.start();
+    Thread.sleep(SOUND_DURATION);
+    clip.stop();
+  }
+
+  private URL getSoundUrl() throws FileNotFoundException {
+    URL soundUrl = this.getClass().getResource(JINGLE_BELLS_FILE);
+    if (soundUrl == null) {
+      throw new FileNotFoundException("File " + JINGLE_BELLS_FILE + "not found");
     }
-
-    private void sleep(Duration timeToSleep) {
-        try {
-            Thread.sleep(timeToSleep.toMillis());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
+    return soundUrl;
+  }
 }
 
